0f2d604a9c36a726c41bbd0ba94296ba4120cc1b
rickkas7 <rickk@rickk.com>
2017-12-27 07:47:44 -0500

    add mustache template generator

--- mustache/main.js
+++ mustache/main.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
 	$('#jsonText').on('keyup blur', handleInputChange);
 	$('#templateText').on('keyup blur', handleInputChange);
 	$('#showJsonDataCheckbox').on('click', handleShowJsonDataCheckbox);
+	$('#copyToTemplateButton').on('click', handleCopyToTemplate);
 	$('#removeLFCheckbox').on('click', handleTestButton);
 	$('#exampleSelect').on('change', handleExampleSelect);
+	
 	
 	// Initialize examples menu
 	for(var ii = 0; ii < examples.length; ii++) {
@@ -46,6 +48,9 @@ function handleTestButton() {
 	try {
 		context = JSON.parse($('#jsonText').val());
 		$('#parseError').hide();	
+
+		$('#decodedDiv').html('');
+		decode(context, 0, false, '');
 	}
 	catch(e) {
 		$('#parseError').show();
@@ -88,7 +93,6 @@ function handleTestButton() {
 	catch(e) {
 		$('#resultJsonFormattedDiv').hide();
 	}
-	
 
 }
 
@@ -100,7 +104,8 @@ function handleShowJsonDataCheckbox() {
 			$('#parseError').hide();	
 			$('#jsonDataFormattedDiv').show();
 			
-			$('#jsonDataFormattedText').val(formatter.formatJson($('#jsonText').val()));				
+			$('#decodedDiv').html('');
+			decode(context, 0, false, '');
 		}
 		catch(e) {
 			$('#parseError').show();
@@ -111,5 +116,109 @@ function handleShowJsonDataCheckbox() {
 	else {
 		$('#jsonDataFormattedDiv').hide();
 	}
+}
+
+function decode(obj, indent, addCommaSeparator, decoder) {
+		
+	if (Array.isArray(obj)) {
+		$('#decodedDiv').append('<div>' + indentSpan(indent) + '[</div>');
+		for(var ii = 0; ii < obj.length; ii++) {
+			decodeValue(obj[ii], indent + 1, null, (ii < (obj.length - 1)), decoder + '.' + ii);
+		}
+		$('#decodedDiv').append('<div>' + indentSpan(indent) + ']' + (addCommaSeparator ? "," : "") + '</div>');
+	}
+	else {
+		$('#decodedDiv').append('<div>' + indentSpan(indent) + '{</div>');
+		var keys = Object.keys(obj);
+		for(var ii = 0; ii < keys.length; ii++) {
+			decodeValue(obj[keys[ii]], indent + 1, keys[ii], (ii < (keys.length - 1)), decoder + '.' + keys[ii]);
+		}
+		$('#decodedDiv').append('<div>' + indentSpan(indent) + '}' + (addCommaSeparator ? "," : "") + '</div>');
+	}
+	
+}
+
+function decodeValue(val, indent, key, addCommaSeparator, decoder) {
+	var html = '';
+	
+	html += indentSpan(indent);
+	if (key) {
+		html += '<span>' + quoteString(key) + ':' + '</span>';		
+	}
+
+	
+	if ((val !== null) && (Array.isArray(val) || typeof val === 'object')) {
+		// Array or object
+		$('#decodedDiv').append(generateCodeDiv(decoder, html));
+		html = '';
+		
+		decode(val, indent + 1, addCommaSeparator, decoder);
+	}
+	else {
+		// Primitive
+		if (typeof val == 'number') {
+			html += '<span>' + val + '</span>';
+		}
+		else
+		if (typeof val == 'string') {
+			html += '<span>' + quoteString(val) + '</span>';
+		}
+		else
+		if (typeof val == 'boolean') {
+			if (val == true) {
+				html += '<span>true</span>';
+			}
+			else {
+				html += '<span>false</span>';
+			}
+		}
+		if (addCommaSeparator) {
+			html += ',';
+		}
+	}
+	
+	if (html != '') {
+		$('#decodedDiv').append(generateCodeDiv(decoder, html));
+	}
+}
+
+function generateCodeDiv(decoder, html) {
+	if (decoder.startsWith('.')) {
+		// Remove leading .; it's easier to do here than special case when adding it
+		decoder = decoder.substr(1);
+	}
+	
+	decoder = '{{' + decoder  + '}}';
+	
+	return '<div data-code="' + decoder.replace(/"/g, '&quot;') + '" onclick="updateCodeDiv(this)">' + html + '</div>';
+}
+
+function quoteString(s) {
+	return '"' + s.replace(/\\/g, '\\\\').replace(/"/g,'\\"') + '"';
+}
+
+function indentSpan(indent) {
+	var result = '<span>';
+	
+	for(var ii = 0; ii < indent; ii++) {
+		result += ' &nbsp; &nbsp;';
+	}
+	
+	result += '</span>';
+	
+	return result;
+}
+
+function updateCodeDiv(obj) {
+	$('.highlight').removeClass('highlight');
+	$(obj).addClass('highlight');
+	
+	$('#codeDiv').html($(obj).attr("data-code"));
+}
+
+function handleCopyToTemplate() {
+	$('#templateText').val($('#templateText').val() + $('#codeDiv').text());
+	handleInputChange();
+	return false;
 }
 
